439ac21f4ce21f15c60eff82f3d8de790ba12831
dunky-star <dukygeoffrey39@gmail.com>
2022-09-27 21:31:36 +0300

    Create TestDbServlet to test Database connectivity

--- web-spring-mvc/src/com/dunky/testdb/TestDbServlet.java
+++ web-spring-mvc/src/com/dunky/testdb/TestDbServlet.java
@@ -1,6 +1,8 @@
 package com.dunky.testdb;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -27,8 +29,33 @@ public class TestDbServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		       // setup connection variables
+				String user = "webstudent";
+				String pass = "webstudent";
+				
+				String jdbcUrl = "jdbc:mysql://localhost:3306/web_customer_tracker?useSSL=false&serverTimezone=GMT+3";
+				String driver = "com.mysql.cj.jdbc.Driver";
+				
+				// get connection to database
+				try {
+					PrintWriter out = response.getWriter();
+					
+					out.println("Connecting to database: " + jdbcUrl);
+					
+					Class.forName(driver);
+					
+					Connection myConn = DriverManager.getConnection(jdbcUrl, user, pass);
+					
+					out.println("SUCCESS!!!");
+					
+					myConn.close();
+					
+				}
+				catch (Exception exc) {
+					exc.printStackTrace();
+					throw new ServletException(exc);
+				}
+			
 	}
 
 }
