0f94ec3d3b715aed8e84940735cd6cd57daec879
dunky-star <dukygeoffrey39@gmail.com>
2022-10-02 12:15:10 +0300

    Add customer DAO interface

---
+++ web-spring-mvc/src/com/dunky/spring/mvc/dao/CustomerDAO.java
@@ -0,0 +1,11 @@
+package com.dunky.spring.mvc.dao;
+
+import java.util.List;
+
+import com.dunky.spring.mvc.entity.Customer;
+
+public interface CustomerDAO {
+	
+	public List<Customer> getCustomers();
+
+}
