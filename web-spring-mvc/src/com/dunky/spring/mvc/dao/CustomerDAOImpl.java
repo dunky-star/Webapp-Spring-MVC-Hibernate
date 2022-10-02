422bc57c1a5d4185b11ea05ea98331b298b01253
dunky-star <dukygeoffrey39@gmail.com>
2022-10-02 13:06:32 +0300

    Add customer DAO interface and DAO Impl

--- web-spring-mvc/src/com/dunky/spring/mvc/dao/CustomerDAOImpl.java
+++ web-spring-mvc/src/com/dunky/spring/mvc/dao/CustomerDAOImpl.java
@@ -1,13 +1,13 @@
 package com.dunky.spring.mvc.dao;
+
 import java.util.List;
 
-import org.hibernate.query.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
-
 import com.dunky.spring.mvc.entity.Customer;
 
 @Repository
@@ -16,10 +16,11 @@ public class CustomerDAOImpl implements CustomerDAO {
 	// need to inject the session factory
 	@Autowired
 	private SessionFactory sessionFactory;
-	
+			
 	@Override
 	@Transactional
 	public List<Customer> getCustomers() {
+		
 		// get the current hibernate session
 		Session currentSession = sessionFactory.getCurrentSession();
 				
@@ -35,3 +36,9 @@ public class CustomerDAOImpl implements CustomerDAO {
 	}
 
 }
+
+
+
+
+
+
