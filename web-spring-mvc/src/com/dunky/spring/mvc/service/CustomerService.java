08bb17b53b397fc8035c0a5ea47c61f56a07b834
dunky-star <dukygeoffrey39@gmail.com>
2022-10-09 11:19:23 +0300

    Add CustomerServivce interface

---
+++ web-spring-mvc/src/com/dunky/spring/mvc/service/CustomerService.java
@@ -0,0 +1,11 @@
+package com.dunky.spring.mvc.service;
+
+import java.util.List;
+
+import com.dunky.spring.mvc.entity.Customer;
+
+public interface CustomerService {
+	
+	public List<Customer> getCustomers();
+
+}
