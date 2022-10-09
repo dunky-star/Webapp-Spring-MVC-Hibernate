62875c063e85090b0cc14cc4e991664928b2ad3f
dunky-star <dukygeoffrey39@gmail.com>
2022-10-09 14:42:35 +0300

    Add customerServivce service and inject into Controller (Autowired).

---
+++ web-spring-mvc/src/com/dunky/spring/mvc/service/CustomerServiceImpl.java
@@ -0,0 +1,26 @@
+package com.dunky.spring.mvc.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.dunky.spring.mvc.dao.CustomerDAO;
+import com.dunky.spring.mvc.entity.Customer;
+
+@Service
+public class CustomerServiceImpl implements CustomerService {
+
+	// We need to inject Customer DAO
+	@Autowired
+	private CustomerDAO customerDAO;
+	
+	@Override
+	@Transactional
+	public List<Customer> getCustomers() {
+		// Delegate call to DAO
+		return customerDAO.getCustomers();
+	}
+
+}
